e0dea67da7a49fcd902c2f993e18d8a55025b294
AidanBraun <11763715-AidanBraun@users.noreply.replit.com>
2023-08-21 22:51:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+let myhttp = require('http');
+
+
+let myserver = myhttp.createServer(
+  function( myrequest, myresponse ) {
+    console.log( myrequest.url );
+
+    let mytext;
+    if ( myrequest.url === "/superSecretPasscode" ) {
+      mytext = "Access granted! Welcome into my web server!";
+    } else {
+      mytext = "Access denied!";
+    }
+    myresponse.writeHead( 200, { "Content-Type": "text/plain" } );
+
+    myresponse.end( mytext );
+  }
+);
+
+myserver.listen(8080, "0.0.0.0");
+
+console.log("myserver has started");
